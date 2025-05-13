66020fea422a721faac3845a040a6615fd43cff2
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-05-13 16:28:28 +0500

    sale schema

---
+++ models/sale.py
@@ -0,0 +1,7 @@
+class Sale:
+    def __init__(self, uid, product_id, quantity, total_price, date):
+        self.uid = uid
+        self.product_id = product_id
+        self.quantity = quantity
+        self.total_price = total_price
+        self.date = date
